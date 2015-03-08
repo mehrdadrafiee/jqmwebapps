d0a75f711e93017ee00a4a30f192086a0ad5e612
mehrdadrafiee <rafiee.mehrdad@gmail.com>
2015-03-08 13:59:20 -0700

    footer buttons function dynamically

--- js/script.js
+++ js/script.js
@@ -2,10 +2,27 @@ $(document).ready(function() {
    $(document).on("pageshow", "[data-role='page']", function() {
    	  if ($($(this)).hasClass("header_default")) {
    	  	$('<header data-theme="b" data-role="header"><h1></h1><a href="#" class="ui-btn-left ui-btn ui-btn-inline ui-mini ui-corner-all ui-btn-icon-notext ui-icon-back" data-rel="back">Back</a><a href="#" class="ui-btn-right ui-btn ui-btn-inline ui-mini ui-corner-all ui-btn-icon-notext ui-icon-info">Info</a></header>')
-   	  	.prependTo( $(this) )
-   	  	.toolbar({ position: "fixed" });
+   	  	  .prependTo( $(this) )
+   	  	  .toolbar({ position: "fixed" });
    	  	$("[data-role='header'] h1").text($(this).jqmData("title"));
    	  } //if header_default
+   	  $.mobile.resetActivePageHeight();
+
+   	  if ($($(this)).hasClass("footer_default")) {
+   	  	$('<footer data-theme="b" data-role="footer" data-position="fixed"><nav data-role="navbar"><ul><li><a href="#home" class="ui-btn ui-icon-home ui-btn-icon-top">Home</a></li><li><a href="#blog" class="ui-btn ui-icon-edit ui-btn-icon-top">Blog</a></li><li><a href="#videos" class="ui-btn ui-icon-video ui-btn-icon-top">Videos</a></li><li><a href="#photos" class="ui-btn ui-icon-camera ui-btn-icon-top">Photos</a></li><li><a href="#tweets" class="ui-btn ui-icon-comment ui-btn-icon-top">Tweets</a></li></ul></nav></footer>')
+   	  	  .appendTo($(this))
+   	  	  .toolbar({position: "fixed"});
+   	  } //if footer_default
+
+   	  var current = $(".ui-page-active").attr('id');
+
+   	  $("[data-role='footer'] a.ui-btn-active").removeClass("ui-btn-active");
+   	  $("[data-role='footer'] a").each(function() {
+   	  	if ($(this).attr('href') === '#' + current) {
+   	  	  $(this).addClass("ui-btn-active");
+   	    }
+   	  });
+
    }); //show_page
 });//document.ready
 
